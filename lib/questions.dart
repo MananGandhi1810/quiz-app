bb89da5ae64a79af83e88c08bb5d415df7fefb74
Manan Gandhi <ardumanan@gmail.com>
2024-12-27 12:10:38 +0530

    Format Code

--- lib/questions.dart
+++ lib/questions.dart
@@ -1,20 +1,23 @@
 class Questions {
   static List questions = [
     {
-      "question": "IKJHKVJLDHIUDCHNLJHCNIUDLSVN SRHVRHNLIVUHNIUHVR HVRUI HIJ H UKL HIUL RNLKEB ISRHN LVHNEX",
-      "options": {1: "", 2: "", 3: "", 4: ""},
+      "question":
+          "IKJHKVJLDHIUDCHNLJHCNIUDLSVN SRHVRHNLIVUHNIUHVR HVRUI HIJ H UKL HIUL RNLKEB ISRHN LVHNEX",
+      "options": {0: "", 1: "", 2: "", 3: ""},
       "answer": 1,
       "difficulty": "easy"
     },
     {
-      "question": "IKJHKVJLDHIUDCHNLJHCNIUDLSVN SRHVRHNLIVUHNIUHVR HVRUI HIJ H UKL HIUL RNLKEB ISRHN LVHNEY",
-      "options": {1: "", 2: "", 3: "", 4: ""},
+      "question":
+          "IKJHKVJLDHIUDCHNLJHCNIUDLSVN SRHVRHNLIVUHNIUHVR HVRUI HIJ H UKL HIUL RNLKEB ISRHN LVHNEY",
+      "options": {0: "", 1: "", 2: "", 3: ""},
       "answer": 1,
       "difficulty": "medium"
     },
     {
-      "question": "IKJHKVJLDHIUDCHNLJHCNIUDLSVN SRHVRHNLIVUHNIUHVR HVRUI HIJ H UKL HIUL RNLKEB ISRHN LVHNEZ",
-      "options": {1: "", 2: "", 3: "", 4: ""},
+      "question":
+          "IKJHKVJLDHIUDCHNLJHCNIUDLSVN SRHVRHNLIVUHNIUHVR HVRUI HIJ H UKL HIUL RNLKEB ISRHN LVHNEZ",
+      "options": {0: "", 1: "", 2: "", 3: ""},
       "answer": 1,
       "difficulty": "hard"
     }
