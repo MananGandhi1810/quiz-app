b67ca86d81b35e1de9171ff1e819409df6747fdf
Manan Gandhi <ardumanan@gmail.com>
2024-12-28 18:29:52 +0530

    Remove difficulty param

--- lib/models/question_model.dart
+++ lib/models/question_model.dart
@@ -1,22 +1,17 @@
-enum Difficulty { easy, medium, hard }
-
 class QuestionModel {
   late String question;
   late List options;
   late String answer;
-  late Difficulty difficulty;
 
   QuestionModel({
     required question,
     required options,
     required answer,
-    required difficulty,
   });
 
   QuestionModel.fromJson(data) {
     question = data['question'];
     options = data['options'];
     answer = data['answer'];
-    difficulty = Difficulty.values.byName(data['difficulty']);
   }
 }
