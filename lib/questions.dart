f1bc8915fc5534ebfd93154dc79ed30735511120
Manan Gandhi <ardumanan@gmail.com>
2024-12-29 12:31:04 +0530

    Remove question difficulty

--- lib/questions.dart
+++ lib/questions.dart
@@ -5,21 +5,18 @@ class Questions {
           "IKJHKVJLDHIUDCHNLJHCNIUDLSVN SRHVRHNLIVUHNIUHVR HVRUI HIJ H UKL HIUL RNLKEB ISRHN LVHNEX",
       "options": ["A", "B", "C", "D"],
       "answer": "A",
-      "difficulty": "easy"
     },
     {
       "question":
           "IKJHKVJLDHIUDCHNLJHCNIUDLSVN SRHVRHNLIVUHNIUHVR HVRUI HIJ H UKL HIUL RNLKEB ISRHN LVHNEY",
       "options": ["A", "B", "C", "D"],
       "answer": "B",
-      "difficulty": "medium"
     },
     {
       "question":
           "IKJHKVJLDHIUDCHNLJHCNIUDLSVN SRHVRHNLIVUHNIUHVR HVRUI HIJ H UKL HIUL RNLKEB ISRHN LVHNEZ",
       "options": ["A", "B", "C", "D"],
       "answer": "C",
-      "difficulty": "hard"
     }
   ];
 }
